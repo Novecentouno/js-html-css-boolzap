81c655d4e58a47fd435af5522761f2f11646dcca
Novecento <ivan.monfrini@gmail.com>
2020-06-17 18:32:35 +0200

    ssasdasdasdasda

--- js/script.js
+++ js/script.js
@@ -3,14 +3,17 @@ $(document).ready(
         $(document).on('click', '.invio_text',
             function() {
             aggiungiMessaggio();
+            setTimeout(computerMessaggio , 1000);
+
         });
         $('.form_messaggio').keypress(
             function(event) {
             if(event.which === 13 || event.keycode === 13) {
                 aggiungiMessaggio();
+                setTimeout(computerMessaggio , 1000);
             }
         });
-
+        // Funzione messaggio inserito utente
         function aggiungiMessaggio() {
             var testoMessaggio = $('.form_messaggio').val();
             if(testoMessaggio != '') {
@@ -27,14 +30,42 @@ $(document).ready(
 
                 $('.finestra_messaggio').append(nuovoMessaggio);
                 $('.form_messaggio').val('');
-                $('.finestra_messaggio').scrollTop($('.finestra_messaggio').height());
+                $('.area_message').scrollTop($('.area_message').height());
             }
         }
+        // Funzione messaggio computer dopo 1 secondo
+        function computerMessaggio() {
+            var testoMessaggio = 'Ok va bene!';
 
+                var nuovoMessaggio = $('.template .messaggio').clone();
+                nuovoMessaggio.children('.message_text').text(testoMessaggio);
+                nuovoMessaggio.addClass('ricevuto');
+
+                var data = new Date();
+                var oraCorrente = data.getHours();
+                var minutoCorrente = data.getMinutes();
+                var oraEsatta = aggiungiLoZero(oraCorrente) + ':' + aggiungiLoZero(minutoCorrente);
+
+                nuovoMessaggio.children('.message_time').text(oraEsatta);
+
+                $('.finestra_messaggio').append(nuovoMessaggio);
+                $('.form_messaggio').val('');
+                $('.area_message').scrollTop($('.area_message').height());
+
+        }
+        // Normalizzare ora corrente
         function aggiungiLoZero(numero) {
             if(numero < 10) {
                 return '0' + numero;
             }
             return numero;
         }
+        // Ricerca Contatti
+        var ricercaVuota = $('.form_ricerca').val('');
+        $('.form_ricerca').click(function(){
+            $('.scheda_contatto').each(function() {
+                $('h5').text();
+            })
+        });
+
 });
